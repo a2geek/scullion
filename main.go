fe3419f68c798150752c64f390d49cfe04f64482
Rob Greene <robgreene@gmail.com>
2020-03-29 18:25:06 -0500

    Experimenting with how to manage features more cleanly.

--- main.go
+++ main.go
@@ -8,44 +8,51 @@ import (
 	"os"
 )
 
-var envName, fileName string
-var verbose bool
-var tasks []Task
-
 func main() {
-	flag.StringVar(&envName, "env", "SCULLION_TASKS", "load configuration from environment `variable`")
-	flag.StringVar(&fileName, "file", "", "read configuration from given `file`")
-	flag.BoolVar(&verbose, "verbose", false, "enable verbose output")
+	envName := flag.String("env", "SCULLION_TASKS", "load configuration from environment `variable`")
+	fileName := flag.String("file", "", "read configuration from given `file`")
+	verbose := flag.Bool("verbose", false, "enable verbose output")
 	flag.Parse()
 
-	config := readConfiguration(envName, fileName)
+	readConfigFunc := evaluateFlags(envName, fileName, verbose)
+
+	config := readConfigFunc()
 	fmt.Println(string(config))
-	tasks = make([]Task, 0)
-	err := json.Unmarshal(config, &tasks)
-	if err != nil {
-		fmt.Printf("Unable to unmarshal configuration: %v\n", err)
-		os.Exit(3)
-	}
+	tasks := loadTasks(config)
 	fmt.Printf("%v\n", tasks)
 }
 
-func readConfiguration(envName string, fileName string) []byte {
-	var config []byte
-	if fileName != "" {
-		data, err := ioutil.ReadFile(fileName)
-		if err != nil {
-			fmt.Printf("Unable to read configuration: %v\n", err)
-			os.Exit(2)
-		}
-		config = data
-	} else if envName != "" {
-		config = []byte(os.Getenv(envName))
+func evaluateFlags(envName, fileName *string, verbose *bool) func() []byte {
+	var readConfigFunc func() []byte
+	if *fileName != "" {
+		readConfigFunc = func() []byte { return readConfigurationFromFile(*fileName) }
+	} else if *envName != "" {
+		readConfigFunc = func() []byte { return []byte(os.Getenv(*envName)) }
 	} else {
 		fmt.Println("Please supply a file or an environment variable for configuration")
 		flag.Usage()
 		os.Exit(1)
 	}
-	return config
+	return readConfigFunc
+}
+
+func loadTasks(config []byte) []Task {
+	tasks := make([]Task, 0)
+	err := json.Unmarshal(config, &tasks)
+	if err != nil {
+		fmt.Printf("Unable to unmarshal configuration: %v\n", err)
+		os.Exit(3)
+	}
+	return tasks
+}
+
+func readConfigurationFromFile(fileName string) []byte {
+	data, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		fmt.Printf("Unable to read configuration: %v\n", err)
+		os.Exit(2)
+	}
+	return data
 }
 
 type Task struct {
