9d067220f796859c01a266396b48fabd6270403a
Rob Greene <robgreene@gmail.com>
2020-05-22 15:33:05 -0500

    Name fix

--- worker/task.go
+++ worker/task.go
@@ -18,8 +18,8 @@ func Task(num int, stateChan chan *ctx.State, wg *sync.WaitGroup, runOpts option
 	// if ctx channel closes, be certain we clean up the wait group!
 	defer wg.Done()
 
-	for ctx := range stateChan {
-		ctx.Execute()
+	for state := range stateChan {
+		state.Execute()
 	}
 
 	logger.Error("exiting")
