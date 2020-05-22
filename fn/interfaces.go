b598bc180b6aa62bdd8fc7b707b77844ef8b8b70
Rob Greene <robgreene@gmail.com>
2020-05-22 15:31:37 -0500

    Comments

--- fn/interfaces.go
+++ fn/interfaces.go
@@ -2,4 +2,8 @@ package fn
 
 import "scullion/ctx"
 
+// Registrar is a registration function to add functions dynamically into a State at runtime.
+// All functions must return a value. If they are _methods_, then they should return error.
+// The execution will look for an error return code, log the message, and stop that particular
+// execution as appropriate.
 type Registrar func(*ctx.State)
